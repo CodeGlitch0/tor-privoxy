7b2348929f238be6a9c4c74b0bb0fdecaa1af0f4
CodeGlitch0 <11563989+CodeGlitch0@users.noreply.github.com>
2019-01-16 20:01:04 +0000

    Added initial docker-compose build

---
+++ prerequisites/install-docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install Docker
+echo Installing Docker...
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce
+
+# Install Docker-compose
+echo Installing Docker-compose...
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Build the Docker images
+echo Starting Docker images...
+docker-compose up -d
+
+echo Tor environment installation is complete.
